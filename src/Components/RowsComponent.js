61c5fed44da1e48e0fbdecc16cc034e51a175466
tejasjadhav1 <74096170+tejasjadhav1@users.noreply.github.com>
2023-07-13 11:01:53 +0530

    Update RowsComponent.js

--- src/Components/RowsComponent.js
+++ src/Components/RowsComponent.js
@@ -1,8 +1,85 @@
 import React from 'react'
+import styles from './RowsComponents.module.css'
+
 
 function RowsComponent() {
   return (
-    <div>RowsComponent</div>
+    <div className={styles['rows_component']}>
+      
+
+      <div className={styles['table_container']}>
+        <table className={styles['table']}>
+          <h3 className={styles['heading']}>Buckets you own</h3>
+          <tr className={styles['table_heading_row']}>
+            <th>Name</th>
+            <th>Budget</th>
+            <th>Spend</th>
+            <th>Forecast</th>
+          </tr>
+
+        <tr className={styles['table_row']}>
+          <td className={styles['table_data']}>ACME Corp</td>
+          <td>$200000</td>
+          <td>$173162</td>
+          <td>$205049</td>
+        </tr>
+        <tr className={styles['table_row']}>
+          <td>ACME Corp</td>
+          <td>$200000</td>
+          <td>$173162</td>
+          <td>$205049</td>
+        </tr>
+        </table>
+      </div >
+      <div className={styles['table_container2']}>
+          <table className={styles['table']}>
+              <h3 className={styles['heading']}>Buckets you own</h3>
+              <tr className={styles['table_heading_row']}>
+                <th>Name</th>
+                <th>Budget</th>
+                <th>Spend</th>
+                <th>Forecast</th>
+              </tr>
+
+            <tr className={styles['table_row']}>
+              <td className={styles['table_data']}>ACME Corp</td>
+              <td>$200000</td>
+              <td>$173162</td>
+              <td>$205049</td>
+            </tr>
+            <tr className={styles['table_row']}>
+              <td>ACME Corp</td>
+              <td>$200000</td>
+              <td>$173162</td>
+              <td>$205049</td>
+            </tr>
+            </table>
+       </div>
+       <div className={styles['table_container3']}>
+          <table className={styles['table']}>
+              <h3 className={styles['heading']}>Buckets you own</h3>
+              <tr className={styles['table_heading_row']}>
+                <th>Name</th>
+                <th>Budget</th>
+                <th>Spend</th>
+                <th>Forecast</th>
+              </tr>
+
+            <tr className={styles['table_row']}>
+              <td className={styles['table_data']}>ACME Corp</td>
+              <td>$200000</td>
+              <td>$173162</td>
+              <td>$205049</td>
+            </tr>
+            <tr className={styles['table_row']}>
+              <td>ACME Corp</td>
+              <td>$200000</td>
+              <td>$173162</td>
+              <td>$205049</td>
+            </tr>
+            </table>
+       </div>
+    </div>
   )
 }
 
